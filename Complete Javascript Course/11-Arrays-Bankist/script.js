18495bf4b3f5c9bf60668f89ba6ee9cb634ef857
Adam Jankanič <71669030+AdamJankanic@users.noreply.github.com>
2022-09-14 13:10:48 +0200

    Login, UI display, Cancel account

--- Complete Javascript Course/11-Arrays-Bankist/script.js
+++ Complete Javascript Course/11-Arrays-Bankist/script.js
@@ -70,7 +70,7 @@ const dispayMovements = function (movements) {
           <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-          <div class="movements__date">3 days ago</div>
+          
           <div class="movements__value">${mov}</div>
         </div>`;
 
@@ -78,8 +78,7 @@ const dispayMovements = function (movements) {
   });
 };
 
-dispayMovements(account1.movements);
-
+// Create username
 function createUserName(nameOfUsers) {
   nameOfUsers.forEach(function (acc) {
     acc.username = acc.owner
@@ -90,35 +89,118 @@ function createUserName(nameOfUsers) {
   });
 }
 
-const callDisplayBalance = function (movements) {
-  const balance = movements.reduce(function (acc, cur, i, array) {
+createUserName(accounts);
+
+function updateUI(loggedAccount) {
+  // Display movements
+  dispayMovements(loggedAccount.movements);
+  // Display balance
+  callDisplayBalance(loggedAccount);
+  // Display summary
+  calcDisplaySummary(loggedAccount);
+}
+
+// Display Balance
+const callDisplayBalance = function (loggedAccount) {
+  loggedAccount.balance = loggedAccount.movements.reduce(function (
+    acc,
+    cur,
+    i,
+    array
+  ) {
     return acc + cur;
-  }, 0);
-  labelBalance.textContent = `${balance}€`;
+  },
+  0);
+  labelBalance.textContent = `${loggedAccount.balance}€`;
 };
 
-callDisplayBalance(account1.movements);
-
-function calcDisplaySummary(movements) {
-  const incomes = movements
+// Display summary
+function calcDisplaySummary(loggedAccount) {
+  const incomes = loggedAccount.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov);
   labelSumIn.textContent = `${incomes}€`;
 
-  const expense = movements
+  const expense = loggedAccount.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov);
   labelSumOut.textContent = `${Math.abs(expense)}€`;
 
-  const interest = movements
+  const interest = loggedAccount.movements
     .filter(mov => mov > 0)
-    .map(deposit => deposit * 0.012)
+    .map(deposit => (deposit * loggedAccount.interestRate) / 100)
     .filter(interests => interests > 1)
     .reduce((acc, mov) => acc + mov);
   labelSumInterest.textContent = `${interest}€`;
 }
 
-calcDisplaySummary(account1.movements);
+// Event handlers
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  // Prevent from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginUsername.blur();
+    inputLoginPin.blur();
+
+    updateUI(currentAccount);
+  }
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+
+  inputTransferAmount.value = inputTransferTo.value = '';
+
+  if (
+    receiverAcc &&
+    amount > 0 &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+
+    updateUI(currentAccount);
+  }
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+    // Delete account
+    accounts.splice(index, 1);
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
