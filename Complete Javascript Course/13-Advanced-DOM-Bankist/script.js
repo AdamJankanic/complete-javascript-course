db1390eff65b15dd438c1e000f4638e3f3905d03
Adam Jankanič <xjankanic@stuba.sk>
2022-09-24 20:35:42 +0200

    Lazy img loading, Slider, Reveal section

--- Complete Javascript Course/13-Advanced-DOM-Bankist/script.js
+++ Complete Javascript Course/13-Advanced-DOM-Bankist/script.js
@@ -152,3 +152,83 @@ const obsOptions = {
 };
 const headerObserver = new IntersectionObserver(stickyNav, obsOptions);
 headerObserver.observe(header);
+
+// Reveal section
+const allSection = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSection.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+// Slider
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+
+goToSlide(0);
+
+btnRight.addEventListener('click', function () {
+  curSlide++;
+
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  }
+
+  goToSlide(curSlide);
+});
+
+btnLeft.addEventListener('click', function () {
+  curSlide--;
+
+  if (curSlide < 0) {
+    curSlide = maxSlide - 1;
+  }
+  goToSlide(curSlide);
+});
