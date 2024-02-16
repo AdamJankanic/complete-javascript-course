31d1c2138b6f853409cfee91949d4f15e17d57fa
Adam Jankanič <xjankanic@stuba.sk>
2024-02-16 13:52:44 +0100

    chapter 14 - class inheritance

--- Complete Javascript Course/14-OOP/starter/script.js
+++ Complete Javascript Course/14-OOP/starter/script.js
@@ -1,69 +1,69 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
-};
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
 
-const jonas = new Person('Jonas', 1991);
-console.log(jonas);
+// const jonas = new Person('Jonas', 1991);
+// console.log(jonas);
 
-const matilda = new Person('Matilda', 2017);
-const jack = new Person('Jack', 1975);
+// const matilda = new Person('Matilda', 2017);
+// const jack = new Person('Jack', 1975);
 
-// Prototypes
-console.log(Person.prototype);
+// // Prototypes
+// console.log(Person.prototype);
 
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
 
-jonas.calcAge();
+// jonas.calcAge();
 
-Person.prototype.species = 'Homo Sapiens';
-console.log(jonas.species);
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(jonas.species);
 
-const Car = function (maker, speed) {
-  this.maker = maker;
-  this.speed = speed;
-};
+// const Car = function (maker, speed) {
+//   this.maker = maker;
+//   this.speed = speed;
+// };
 
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-};
-Car.prototype.brake = function () {
-  this.speed -= 5;
-};
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+// };
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+// };
 
-const car1 = new Car('BMW', 120);
-const car2 = new Car('Mercedes', 95);
-console.log(car1);
-car1.brake();
-car1.accelerate();
-console.log(car1);
+// const car1 = new Car('BMW', 120);
+// const car2 = new Car('Mercedes', 95);
+// console.log(car1);
+// car1.brake();
+// car1.accelerate();
+// console.log(car1);
 
-console.log(car2);
-car2.brake();
-car2.accelerate();
-console.log(car2);
+// console.log(car2);
+// car2.brake();
+// car2.accelerate();
+// console.log(car2);
 //
 //
 //
 //
 
-class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-  }
+// class PersonCl {
+//   constructor(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   }
 
-  calcAge() {
-    console.log(2037 - this.birthYear);
-  }
-}
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   }
+// }
 
-const jessica = new PersonCl('Jessica', 1996);
-console.log(jessica);
+// const jessica = new PersonCl('Jessica', 1996);
+// console.log(jessica);
 
 class CarCl {
   constructor(maker, speed) {
@@ -88,3 +88,86 @@ ford.speedUs;
 console.log(ford);
 ford.speedUs = 50;
 console.log(ford);
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+mike.introduce();
+
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(firstName, birthYear, course) {
+    super(firstName, birthYear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
+  }
+}
+
+const miro = new StudentCl('Mirko', 2012, 'Computer Science');
+miro.introduce();
+miro.calcAge();
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thank's ${owner}!`);
+  }
+
+  deposit(value) {
+    this.movements.push(value);
+  }
+
+  withdraw(value) {
+    this.deposit(-value);
+  }
+
+  approvedLoan(val) {
+    return true;
+  }
+
+  requestLoan(value) {
+    if (this.approvedLoan) {
+      this.deposit(value);
+      console.log('Approved');
+    }
+  }
+}
+
+const acc1 = new Account('Adam', 'eur', 1234);
+acc1.deposit(250);
+acc1.withdraw(140);
+console.log(acc1);
